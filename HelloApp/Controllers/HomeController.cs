658c59bf7d02838df17fc24d38e284efbdf4f2c1
Reyaz Ahmad <Reyaz.Ahmad.ext@unit4.com>
2022-01-22 19:20:49 +0530

    hello app

---
+++ HelloApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloApp.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public IActionResult Get()
+        {
+           return Ok("Hello Reyaz!!!");
+        }
+
+        [Route("{name}")]
+        public IActionResult Get(string name)
+        {
+            return Ok($"Hello {name}!!!");
+        }
+    }
+}
---
+++ HelloApp/readme.txt
@@ -0,0 +1,7 @@
+go to root folder
+open cmd
+run following command
+> docker build . -t helloapp
+
+thn run following command
+> docker run -p 9000:80 helloapp
